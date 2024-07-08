52f3ca84178b533da56e417d23c4d2e6f883a734
lsh02002 <lsh02002@hanmail.net>
2024-07-08 12:37:59 +0900

    add user-토큰 필요할 때 메시지

--- src/main/java/com/example/superproject1/web/controller/auth/AuthController.java
+++ src/main/java/com/example/superproject1/web/controller/auth/AuthController.java
@@ -2,6 +2,8 @@ package com.example.superproject1.web.controller.auth;
 
 import com.example.superproject1.repository.users.userDetails.CustomUserDetails;
 import com.example.superproject1.service.authService.AuthService;
+import com.example.superproject1.service.exceptions.AccessDeniedException;
+import com.example.superproject1.service.exceptions.NotAcceptableException;
 import com.example.superproject1.web.dto.auth.AuthResponseDto;
 import com.example.superproject1.web.dto.auth.LoginRequest;
 import com.example.superproject1.web.dto.auth.SignupRequest;
@@ -30,8 +32,25 @@ public class AuthController {
         return (AuthResponseDto) tokenAndResponse.get(1);
     }
 
-    @GetMapping("/test")
-    public Object test(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+    @GetMapping(value = "/entrypoint")
+    public void entrypointException(@RequestParam(name = "token", required = false) String token) {
+        if (token==null) throw new NotAcceptableException("로그인(Jwt 토큰)이 필요합니다.", null);
+        else throw new NotAcceptableException("로그인이 만료 되었습니다.","유효하지 않은 토큰 : "+ token);
+    }
+
+    @GetMapping(value = "/access-denied")
+    public void accessDeniedException(@RequestParam(name = "roles", required = false) String roles) {
+        if(roles==null) throw new AccessDeniedException("권한이 설정되지 않았습니다.",null);
+        else throw new AccessDeniedException("권한이 없습니다.", "시도한 유저의 권한 : "+roles);
+    }
+
+    @GetMapping("/test1")
+    public Object test1(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         return customUserDetails.toString();
+    }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "Jwt 토큰이 상관없는 EndPoint 테스트입니다.";
     }
 }
